551d77cb5e4f7e9e0c3958a4e7e8cafa472fc62e
Nertinia <60277348+Nertinia@users.noreply.github.com>
2020-05-08 12:18:06 +0800

    q

---
+++ main.js
@@ -0,0 +1,159 @@
+function d1(){
+  document.getElementById('pc1').style.display="none";
+}
+function d2(){
+  document.getElementById('pc2').style.display="none";
+}
+function d3(){
+  document.getElementById('pc3').style.display="none";
+}
+function d4(){
+  document.getElementById('pc4').style.display="none";
+}
+function d5(){
+  document.getElementById('pc5').style.display="none";
+}
+function d6(){
+  document.getElementById('pc6').style.display="none";
+}
+function d7(){
+  document.getElementById('pc7').style.display="none";
+}
+function d8(){
+  document.getElementById('pc8').style.display="none";
+}
+function d9(){
+  document.getElementById('pc9').style.display="none";
+}
+function d10(){
+  document.getElementById('pc10').style.display="none";
+}
+function d11(){
+  document.getElementById('pc11').style.display="none";
+}
+function d12(){
+  document.getElementById('pc12').style.display="none";
+}
+function d13(){
+  document.getElementById('pc13').style.display="none";
+}
+function d14(){
+  document.getElementById('pc14').style.display="none";
+}
+function d15(){
+  document.getElementById('pc15').style.display="none";
+}
+function d16(){
+  document.getElementById('pc16').style.display="none";
+}
+function d17(){
+  document.getElementById('pc17').style.display="none";
+}
+function d18(){
+  document.getElementById('pc18').style.display="none";
+}
+function d19(){
+  document.getElementById('pc19').style.display="none";
+}
+function d20(){
+  document.getElementById('pc20').style.display="none";
+}
+function d21(){
+  document.getElementById('pc21').style.display="none";
+}
+function d22(){
+  document.getElementById('pc22').style.display="none";
+}
+function d23(){
+  document.getElementById('pc23').style.display="none";
+}
+function d24(){
+  document.getElementById('pc24').style.display="none";
+}
+function d25(){
+  document.getElementById('pc25').style.display="none";
+}
+function d26(){
+  document.getElementById('pc26').style.display="none";
+}
+function d27(){
+  document.getElementById('pc27').style.display="none";
+}
+function d28(){
+  document.getElementById('pc28').style.display="none";
+}
+function d29(){
+  document.getElementById('pc29').style.display="none";
+}
+function d30(){
+  document.getElementById('pc30').style.display="none";
+}
+function d31(){
+  document.getElementById('pc31').style.display="none";
+}
+function d32(){
+  document.getElementById('pc32').style.display="none";
+}
+function d33(){
+  document.getElementById('pc33').style.display="none";
+}
+function d34(){
+  document.getElementById('pc34').style.display="none";
+}
+function d35(){
+  document.getElementById('pc35').style.display="none";
+}
+function d36(){
+  document.getElementById('pc36').style.display="none";
+}
+function d37(){
+  document.getElementById('pc37').style.display="none";
+}
+function d38(){
+  document.getElementById('pc38').style.display="none";
+}
+function d39(){
+  document.getElementById('pc39').style.display="none";
+}
+function d40(){
+  document.getElementById('pc40').style.display="none";
+}
+function d41(){
+  document.getElementById('pc41').style.display="none";
+}
+function d42(){
+  document.getElementById('pc42').style.display="none";
+}
+function d43(){
+  document.getElementById('pc43').style.display="none";
+}
+function d44(){
+  document.getElementById('pc44').style.display="none";
+}
+function d45(){
+  document.getElementById('pc45').style.display="none";
+}
+function d46(){
+  document.getElementById('pc46').style.display="none";
+}
+function d47(){
+  document.getElementById('pc47').style.display="none";
+}
+function d48(){
+  document.getElementById('pc48').style.display="none";
+}
+function d49(){
+  document.getElementById('pc49').style.display="none";
+}
+function d50(){
+  document.getElementById('pc50').style.display="none";
+}
+function d51(){
+  document.getElementById('pc51').style.display="none";
+}
+function d52(){
+  document.getElementById('pc52').style.display="none";
+}
+function d53(){
+    document.getElementById('pc53').style.display="none";
+  }
